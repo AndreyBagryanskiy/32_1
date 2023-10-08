ea32c39c68eea1ebcbef4aab93963427126bc994
Andrey Bagryanskiy <andrey@1dist.ru>
2023-10-08 22:58:05 +0300

    edit main.cpp

--- main.cpp
+++ main.cpp
@@ -1,39 +1,30 @@
 #include<iostream>
 #include<string>
 #include<fstream>
-#include<vector>
 #include"nlohmann/json.hpp"
-#include<ctime>
-#include<iomanip>
 
-struct film
-{
-    std::string filmName;
-    std::string Country;
-    std::string directedBy;
-    int age;
-    bool married;
-};
 
 int main(){
-    std::time_t t = std::time(nullptr);
-    std::tm local = *std::localtime(&t);
+    nlohmann::json film = {
+        {"film name", "White Tiger (2012 film)"},
+        {"release date", "2023/10/08"},
+        {"Country", "Russia"},
+        {"Production companies", "Mosfilm"},
+        {"Directed by", "Karen Shakhnazarov"},
+        {"Screenplay by", "Aleksandr Borodyansky"},
+        {"actors", {
+            {"Aleksey Vertkov", "Ivan Naydenov"},
+            {"Vitali Kishchenko", "Major Fedotov"},
+            {"Gerasim Arkhipov",  "Captain Sharipov"},
+            {"Aleksandr Bakhov", "Kryuk"}
+        }}
+
+    };
+
+
 
     std::ofstream file("film.json");
-    nlohmann::json film;
-    film["date"] = std::asctime(&local);
-    
-
     file << film;
     file.close();
-    /*
-    Record record;
-    std::cin >> record.name >> record.family >> record.age >> record.married;
-    std::ofstream file("record.json");
-    nlohmann::json dict;
-    dict["name"] = record.name;
-    dict["family"] = record.family;
-    dict["age"] = record.age;
-    dict["married"] = record.married;
-    file << dict; */
+ 
 }
