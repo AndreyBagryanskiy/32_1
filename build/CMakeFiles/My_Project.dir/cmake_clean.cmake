e534ac55e78544fd786bcbea906197d2a13dd928
Andrey Bagryanskiy <andrey@1dist.ru>
2024-01-06 23:58:28 +0300

    1

---
+++ build/CMakeFiles/My_Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/My_Project.dir/main.cpp.obj"
+  "CMakeFiles/My_Project.dir/main.cpp.obj.d"
+  "My_Project.exe"
+  "My_Project.exe.manifest"
+  "My_Project.pdb"
+  "libMy_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/My_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
